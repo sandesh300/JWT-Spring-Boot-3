117ca4a6d158eeb1f706d322bcd2b1ef04154616
sandesh <bhujbalsandesh52@gmail.com>
2024-03-28 20:42:50 +0530

    update register api, fix some error

--- src/main/java/com/jwt/example/controller/AuthenticationController.java
+++ src/main/java/com/jwt/example/controller/AuthenticationController.java
@@ -5,17 +5,19 @@ import com.jwt.example.model.User;
 import com.jwt.example.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth/v1/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
+    @PostMapping("/signup")
     public ResponseEntity<User> signup(@RequestBody SignUpRequest signUpRequest){
         return ResponseEntity.ok(authenticationService.signup(signUpRequest));
     }
--- src/main/java/com/jwt/example/controller/UserController.java
+++ src/main/java/com/jwt/example/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth/v1/user")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 }
